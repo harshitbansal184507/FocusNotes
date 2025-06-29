39f5093288d3fe51143f11670432030c6a6ce795
Harshit Bansal <84428515+harshitbansal184507@users.noreply.github.com>
2025-06-29 23:48:27 +0530

    frontend dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,15 @@
 import React from "react"
+import {BrowserRouter , Routes , Route , Navigate} from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+function logout{
+  localStorage.clear()
+  return <Navigate to= "/login" />
+}
 
 function App() {
 
